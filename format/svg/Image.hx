d8bc24c1d8dabb822b39efab2792b2a83e1991bf
guruas3 <guru.as3@gmail.com>
2020-02-24 12:07:12 +0100

    Add copyDataToBitmap method inside Image class as it doesn't need any external references

--- format/svg/Image.hx
+++ format/svg/Image.hx
@@ -6,16 +6,29 @@ import openfl.display.Bitmap;
 
 class Image
 {
-   public function new() { }
 
-   public var name:String;
-   public var matrix:Matrix;
-   public var x:Float;
-   public var y:Float;
-   public var width:Float;
-   public var height:Float;
-   public var href:String;
-   public var bitmap:Bitmap;
-   public var visible:Bool;
-   public var parentGroupName:String;
+	public var name:String;
+	public var matrix:Matrix;
+	public var x:Float;
+	public var y:Float;
+	public var width:Float;
+	public var height:Float;
+	public var href:String;
+	public var bitmap:Bitmap;
+	public var visible:Bool;
+	public var parentGroupName:String;
+
+	public function new() {}
+
+	public function copyDataToBitmap():Void {
+		bitmap.smoothing = true;
+		bitmap.x = x;
+		bitmap.y = y;
+		bitmap.width = width;
+		bitmap.height = height;
+		var m = bitmap.transform.matrix;
+		m.concat(matrix);
+		bitmap.transform.matrix = m;
+	}
+
 }
--- format/svg/SVGRenderer.hx
+++ format/svg/SVGRenderer.hx
@@ -36,7 +36,7 @@ class SVGRenderer
     public var height(default,null):Float;
     public var baseImagePath:String = "";
     public var imageDependencies:Map<String, BitmapData>;
-	 public var loadImageCallback:Image->Void;
+	public var loadImageCallback:Image->Void;
 
     var mSvg:SVGData;
     var mRoot:Group;
@@ -106,45 +106,32 @@ class SVGRenderer
        
 		if (parent != null && inImage.visible) {
 
-         if (inImage.bitmap.bitmapData == null) {
+			if (inImage.bitmap.bitmapData == null) {
 
-            if (imageDependencies.exists(inImage.href)) {
-               var imageDependency = imageDependencies[inImage.href];
+				if (imageDependencies.exists(inImage.href)) {
+					var imageDependency = imageDependencies[inImage.href];
 
-               if (imageDependency != null) {
-                  inImage.bitmap.bitmapData = imageDependency;
-                  copyToBitmap(inImage);
-               }
-            } else {
-               if (StringTools.startsWith(inImage.href, "data:")) {
-                  // Data URI for image bytes
-                  var mimeType = inImage.href.split(";")[0].substr(5);
-                  var imageBytes = haxe.crypto.Base64.decode(inImage.href.substr( inImage.href.indexOf(",") + 1));
+					if (imageDependency != null) {
+						inImage.bitmap.bitmapData = imageDependency;
+						inImage.copyDataToBitmap();
+					}
+				} else {
+					if (StringTools.startsWith(inImage.href, "data:")) {
+						// Data URI for image bytes
+						var mimeType = inImage.href.split(";")[0].substr(5);
+						var imageBytes = haxe.crypto.Base64.decode(inImage.href.substr(inImage.href.indexOf(",") + 1));
 
-                  //TODO:
-               } else {
-                  imageDependencies[inImage.href] = null;
-                  if (loadImageCallback != null) loadImageCallback(inImage);
-                  
-               }
-            }
-         } else {
-            if (loadImageCallback != null) loadImageCallback(inImage);
-         }
+						//TODO:
+					} else {
+						imageDependencies[inImage.href] = null;
+						if (loadImageCallback != null) loadImageCallback(inImage);
+					}
+				}
+			} else {
+				if (loadImageCallback != null) loadImageCallback(inImage);
+			}
 		}
 	}
-
-    private function copyToBitmap(image:Image) {
-		image.bitmap.smoothing = true;
-		image.bitmap.name = image.name;
-		image.bitmap.x = image.x;
-		image.bitmap.y = image.y;
-		image.bitmap.width = image.width;
-		image.bitmap.height = image.height;
-		var m = image.bitmap.transform.matrix;
-		m.concat(image.matrix);
-		image.bitmap.transform.matrix = m;
-    }
    
     public function iteratePath(inPath:Path)
     {
